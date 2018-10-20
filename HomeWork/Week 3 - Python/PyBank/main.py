6cd183c93443d5dec20e84d7c091edadb8746686
TinGri <catchvivkrish@gmail.com>
2018-10-20 04:09:11 -0700

    no message

---
+++ HomeWork/Week 3 - Python/PyBank/main.py
@@ -0,0 +1,68 @@
+#The dataset is composed of two columns: Date and Profit/Losses.
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+#The total number of months included in the dataset
+#The total net amount of "Profit/Losses" over the entire period
+#The average change in "Profit/Losses" between months over the entire period
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period
+#  Financial Analysis
+#  ----------------------------
+#  Total Months: 86
+#  Total: $38382578
+#  Average  Change: $-2315.12
+#  Greatest Increase in Profits: Feb-2012 ($1926159)
+#  Greatest Decrease in Profits: Sep-2013 ($-2196167)
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+def main():
+    import os
+    import csv
+    import sys
+    csvpath = os.path.join("budget_data.csv")
+    total_months = int(0)
+    total_profits = int(0)
+    list = []
+    list2 = []
+    line_average = []
+    with open(csvpath, newline="") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        firstline = True
+        #list = [row[1] for row in csvreader]
+        #list.remove('Profit/Losses')
+        for row in csvreader:
+            if firstline:  # skip first line which is the header
+                firstline = False
+                continue
+            total_months += 1
+            total_profits = total_profits + int(row[1])
+            list.append(row[1])
+            list2.append(row[0])
+
+        for item in range(len(list)-1):
+            average = (int(list[item+1])-int(list[item]))
+            line_average.append(average)
+        final_average = sum(line_average)/len(line_average)
+        final_average = round(final_average,2)
+
+        print("Financial Analysis")
+        print("----------------------------")
+        print ("Total Months: " + str(total_months))
+        print ("Total Profit: " + str(total_profits))
+        print ("Average  Change:" + str(final_average))
+        print("Greatest Increase in Profits: " + str(list2[line_average.index(max(line_average))+1]) + " " +
+              str(max(line_average)))
+        print("Greatest Decrease in Profits: " + str(list2[line_average.index(min(line_average))+1]) + " " +
+              str(min(line_average)))
+
+        sys.stdout = open('PyBank_Results.txt', 'w')
+        print("Financial Analysis")
+        print("----------------------------")
+        print("Total Months: " + str(total_months))
+        print("Total Profit: " + str(total_profits))
+        print("Average  Change:" + str(final_average))
+        print("Greatest Increase in Profits: " + str(list2[line_average.index(max(line_average)) + 1]) + " " +
+              str(max(line_average)))
+        print("Greatest Decrease in Profits: " + str(list2[line_average.index(min(line_average)) + 1]) + " " +
+              str(min(line_average)))
+        sys.stdout.close()
+main()
---
+++ HomeWork/Week 3 - Python/PyPoll/main.py
@@ -0,0 +1,71 @@
+#You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate.
+# Your task is to create a Python script that analyzes the votes and calculates each of the following:
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
+#As an example, your analysis should look similar to the one below:
+#  Election Results
+#  -------------------------
+#  Total Votes: 3521001
+#  -------------------------
+#  Khan: 63.000% (2218231)
+#  Correy: 20.000% (704200)
+#  Li: 14.000% (492940)
+#  O'Tooley: 3.000% (105630)
+#  -------------------------
+#  Winner: Khan
+#  -------------------------
+
+def main():
+    import os
+    import csv
+    import sys
+    csvpath = os.path.join("election_data.csv")
+    total_votes = int(0)
+    list1 = {}
+
+    with open(csvpath, newline="") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        firstline = True
+        for row in csvreader:
+            if firstline:  # skip first line which is the header
+                 firstline = False
+                 continue
+            total_votes += 1
+            if row[2] not in list1:
+                list1[row[2]] = 1
+            else:
+                list1[row[2]] += 1
+        print("Election Results")
+        print("----------------------------")
+        print ("Total Votes: " + str(total_votes))
+        print("----------------------------")
+        list2 = list(list1.keys())
+        list3 = list(list1.values())
+
+        for item in range(len(list2)):
+            perc = round(((list3[item]/sum(list3))*100),2)
+            print(str(list2[item]) + ": " +str(perc) + "%" + " (" + str(list3[item]) + ")")
+        print("----------------------------")
+        print("Winner: "+ str(list2[list3.index(max(list3))]))
+        print("----------------------------")
+
+        sys.stdout = open('PyPoll_Results.txt', 'w')
+        print("Election Results")
+        print("----------------------------")
+        print ("Total Votes: " + str(total_votes))
+        print("----------------------------")
+        list2 = list(list1.keys())
+        list3 = list(list1.values())
+
+        for item in range(len(list2)):
+            perc = round(((list3[item]/sum(list3))*100),2)
+            print(str(list2[item]) + ": " +str(perc) + "%" + "(" + str(list3[item]) + ")")
+        print("----------------------------")
+        print("Winner: "+ str(list2[list3.index(max(list3))]))
+        print("----------------------------")
+        sys.stdout.close()
+main()

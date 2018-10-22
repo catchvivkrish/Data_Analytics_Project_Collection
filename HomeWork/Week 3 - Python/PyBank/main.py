ba513395023a595192ca02246cf0f9b280754ace
TinGri <catchvivkrish@gmail.com>
2018-10-21 17:13:25 -0700

    no message

--- HomeWork/Week 3 - Python/PyBank/main.py
+++ HomeWork/Week 3 - Python/PyBank/main.py
@@ -1,23 +1,6 @@
-#The dataset is composed of two columns: Date and Profit/Losses.
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
-#The total number of months included in the dataset
-#The total net amount of "Profit/Losses" over the entire period
-#The average change in "Profit/Losses" between months over the entire period
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-#  Financial Analysis
-#  ----------------------------
-#  Total Months: 86
-#  Total: $38382578
-#  Average  Change: $-2315.12
-#  Greatest Increase in Profits: Feb-2012 ($1926159)
-#  Greatest Decrease in Profits: Sep-2013 ($-2196167)
-#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
 def main():
     import os
     import csv
-    import sys
     csvpath = os.path.join("budget_data.csv")
     txtpath_out = os.path.join("PyBank_Results.txt")
     total_months = int(0)
@@ -29,16 +12,6 @@ def main():
         csvreader = csv.reader(csvfile, delimiter=",")
         if csv.Sniffer().has_header(open(csvpath).read(1024)):
             next(csvreader)
-        """
-        firstrow = True
-            while firstrow:
-                firstrow = False
-                next(csvreader)
-                continue
-        """
-        #list = [row[1] for row in csvreader]
-        #list.remove('Profit/Losses')
-        #firstrow = True
         for row in csvreader:
             total_months += 1
             total_profits = total_profits + float(row[1])
@@ -63,7 +36,4 @@ def main():
 
         with open(txtpath_out, 'w') as txtfile:
             txtfile.write(print_content)
-        #sys.stdout = open('PyBank_Results.txt', 'w')
-        #print(print_content)
-        #sys.stdout.close()
 main()

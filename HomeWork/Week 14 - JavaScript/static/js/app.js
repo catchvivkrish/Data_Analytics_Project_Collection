763dd8bfba9a09ffe1b2f17ccb08e638770a735c
Viv Krish <catchvivkrish@gmail.com>
2019-01-26 18:53:04 -0800

    no message

--- HomeWork/Week 14 - JavaScript/static/js/app.js
+++ HomeWork/Week 14 - JavaScript/static/js/app.js
@@ -15,24 +15,55 @@ dataset.forEach( (dataset) => {
     col.text(value)
     })
 })
+// Obrain the row count of the filtered data and print on screen
+var record_count = CountRows()
+document.getElementById("row-count").innerHTML = "Total records found: " + record_count;
 }
+
+// function filter(type,val) {
+//     console.log(type)
+//     console.log(val)
+//     console.log(ufoData)
+//     var fil = ufoData.filter(value => value.type === val)
+//     console.log(fil)
+//    // console.log(filtered_data)
+// }
 
 //Display all data on page loan
 display(ufoData)
-
 //Take action on click on submit
 var submit = d3.select('#filter-btn')
 submit.on('click', function() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    // Select the filter_date and get the raw HTML node
-    var filter_date = d3.select('#datetime');
-    // Get the value property of the filter_date
-    var filter_date_value = filter_date.property("value");
-    // Filter the data from original data set based on the date entered by the user
-    var filtered_data = ufoData.filter((value) => value.datetime === filter_date_value)
+    // Obtain the filter type
+    var filter_type = d3.select('#filter_type').property('value')
+    // Obtain the filter value
+    var filter_value = d3.select('#filter_value').property("value")
+    // check the type of filter and apply the respective filter on the base dataset
+    if (filter_type === 'date'){
+       var filtered_data = ufoData.filter((value) => value.datetime === filter_value)
+    }
+    else if(filter_type === 'city'){
+        var filtered_data = ufoData.filter((value) => value.city === filter_value)
+    }
+    else if(filter_type === 'state'){
+        var filtered_data = ufoData.filter((value) => value.state === filter_value)
+    }
+    else if(filter_type === 'country'){
+        var filtered_data = ufoData.filter((value) => value.country === filter_value)
+    }
+    else if (filter_type === 'shape'){
+        var filtered_data = ufoData.filter((value) => value.shape === filter_value)
+    }
+    else {
+        // Set the value property of the filter_date to blank
+        d3.select('#filter_value').property('value', '')
+        // Show the full data set
+        var filtered_data = ufoData
+    }
     // check if filtered date is empty
-    if (filter_date_value !== ''){
+    if (filter_value !== ''){
         // check if record exists in dataset post filtering by date
         if (filtered_data.length>0){
             //call function to print filtered data
@@ -60,5 +91,19 @@ clear.on('click',function() {
     // call function to show all data 
     display(ufoData)
     // Set the value property of the filter_date to blank
-    d3.select('#datetime').property('value', '')
+    d3.select('#filter_value').property('value', '')
 })
+
+function CountRows() {
+    var totalRowCount = 0;
+    var rowCount = 0;
+    var table = document.getElementById("ufo-table");
+    var rows = table.getElementsByTagName("tr")
+    for (var i = 0; i < rows.length; i++) {
+        totalRowCount++;
+        if (rows[i].getElementsByTagName("td").length > 0) {
+            rowCount++;
+        }
+    }
+    return rowCount
+}

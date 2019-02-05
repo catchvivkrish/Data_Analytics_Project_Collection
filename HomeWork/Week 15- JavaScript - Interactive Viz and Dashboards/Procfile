7e8f79aab6e0d5f19b95b5aee55735fce2f88cf9
Viv Krish <catchvivkrish@gmail.com>
2019-02-04 19:18:50 -0800

    no message

---
+++ HomeWork/Week 15- JavaScript - Interactive Viz and Dashboards/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bbd.app:app
--- HomeWork/Week 15- JavaScript - Interactive Viz and Dashboards/bbd/app.py
+++ HomeWork/Week 15- JavaScript - Interactive Viz and Dashboards/bbd/app.py
@@ -15,8 +15,12 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/bellybutton.sqlite"
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 db = SQLAlchemy(app)
+
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+#db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
 Base = automap_base()
